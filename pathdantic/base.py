a60913a3eb2bc7df2acb42f41a850191d0914aae
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-03-10 00:56:16 -0300

    PathBase

--- pathdantic/base.py
+++ pathdantic/base.py
@@ -1,6 +1,10 @@
-from pydantic import BaseModel
+from pathlib import Path
+
+from pydantic import BaseModel, Field
 
 class PathBase(BaseModel):
+    p: Path = Field(frozen=True)
+
     def mkdir(self, mode: int = 511, parents: bool = False, exist_ok: bool = True) -> None:
         self.p.mkdir(mode=mode, parents=parents, exist_ok=exist_ok)
 
